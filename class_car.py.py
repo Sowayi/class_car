0136a637fda0c1d69c3511eaee916a39ace59bd1
Sowayi <pasowayi@gmail.com>
2017-06-14 02:15:37 +0300

    adds the class_car program and a readme file

---
+++ class_car.py.py
@@ -0,0 +1,34 @@
+class car(object):
+
+    def _init_ (self, type='honda',model='GM',name='General'):
+        self.type=car_type
+        self.model=car_model
+        self.name=car_name
+        
+    if car_name = None:
+        return 'General'
+    if car_model = None:
+        return 'GM'
+     
+    def car_properties(self, car_name,car_model):
+        self.car_name.car_model=car_properties
+
+    def car_doors(self,car_name):
+        self.car_name= number_of_doors
+        if car_name == 'Porsche' or car_name == 'Koenigsegg':
+            number_of_doors = 2
+        else:
+            number_of_doors = 4
+
+    def car_type(self, car_type):
+        if car_type != 'Trailer':
+           car_type == 'Saloon'
+
+    def drive(self,speed,moving_man):
+        if car_type== 'Trailer'
+            self.speed= speed * 11
+        elif:
+            self.speed= speed * 10
+        else:
+            return moving_man
+            
